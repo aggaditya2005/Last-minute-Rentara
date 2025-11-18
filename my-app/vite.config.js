7810d0d1e7369d13864828fff430315877e75e43
utkarsh <156888342+Arsenic-23@users.noreply.github.com>
2025-11-18 17:45:39 +0530

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,25 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import history from "connect-history-api-fallback";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "single-page-app",
-      configureServer(server) {
-        server.middlewares.use(
-          history({
-            index: "/index.html",
-            htmlAcceptHeaders: ["text/html", "application/xhtml+xml"],
-            disableDotRule: false,
-            verbose: false,
-          })
-        );
-      },
-    },
-  ],
-
+  plugins: [react()],
   server: {
     port: 3000,
     host: true,
