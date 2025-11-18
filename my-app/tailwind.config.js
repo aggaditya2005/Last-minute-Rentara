ea9aa71f9988446534a1e791b87a2022b459e5aa
utkarsh <156888342+Arsenic-23@users.noreply.github.com>
2025-11-18 22:34:53 +0530

    Update tailwind.config.js

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,24 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       keyframes: {
         glow: {
-          '0%': {
-            textShadow: '0 0 10px #ff4d6d, 0 0 20px #ff99b3, 0 0 40px #ffccd6',
+          "0%": {
+            textShadow:
+              "0 0 10px #ff4d6d, 0 0 20px #ff99b3, 0 0 40px #ffccd6",
           },
-          '100%': {
-            textShadow: '0 0 20px #ff4d6d, 0 0 30px #ff99b3, 0 0 60px #ffccd6, 0 0 80px #ffccd6',
+          "100%": {
+            textShadow:
+              "0 0 20px #ff4d6d, 0 0 30px #ff99b3, 0 0 60px #ffccd6, 0 0 80px #ffccd6",
           },
         },
       },
       animation: {
-        glow: 'glow 2s ease-in-out infinite alternate',
+        glow: "glow 2s ease-in-out infinite alternate",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
